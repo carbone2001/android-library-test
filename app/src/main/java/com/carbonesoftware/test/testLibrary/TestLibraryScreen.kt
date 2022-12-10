2e336ebfe9a47d4ad73d0637a9ce8a072241dabe
Lucas Tomas Carbone <zextlcarbone@correoargentino.com.ar>
2022-12-10 10:44:10 -0300

    fix: Library version

--- app/src/main/java/com/carbonesoftware/test/testLibrary/TestLibraryScreen.kt
+++ app/src/main/java/com/carbonesoftware/test/testLibrary/TestLibraryScreen.kt
@@ -5,11 +5,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.image_preview.ImagePreview
 
 @Composable
 fun TestLibraryScreen(){
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-        ImagePreview()
+
     }
 }
