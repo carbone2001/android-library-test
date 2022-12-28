52d92c2049f4be9a6db647f67c00c64bdea2cf7a
carbone2001 <lucascarbone2000@gmail.com>
2022-12-28 08:19:24 -0300

    Gradle & color pallete & fonts

--- app/src/main/java/com/example/investigation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/investigation/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -13,9 +14,19 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = BrunswickGreen,
+    primaryVariant = Artichoke,
+    secondary = FernGreen,
+    background = MistyRose,
+    surface = MistyRose,
+//    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+
+//    primary = Purple500,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -32,11 +43,12 @@ fun InvestigationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
--- app/src/main/java/com/example/investigation/ui/theme/Type.kt
+++ app/src/main/java/com/example/investigation/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package com.example.investigation.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.investigation.R
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
